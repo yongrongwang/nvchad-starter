d5c48cd095e83262ec4a42cfadc9f4cd092cd934
yongrongwang <2278508989@qq.com>
2024-05-18 18:30:50 +0800

    Add LSP for markdown, python and javascript, update UI config
    Signed-off-by: yongrongwang <2278508989@qq.com>

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,9 +2,10 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = {
+  "html", "cssls", "marksman", "ts_ls", "pyright", "bashls",
+  "terraformls"
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -15,10 +16,3 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
