f7c1f4bd9f40f520ed5acdf0f967a35b40134ddc
yongrongwang <2278508989@qq.com>
2024-05-18 18:30:50 +0800

    Add LSP for markdown and javascript, update UI config

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+local bo = vim.bo
+o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true
+o.signcolumn = "auto"
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "html" },
+  callback = function()
+    bo.shiftwidth = 4
+    bo.tabstop = 4
+    bo.softtabstop = 4
+  end
+})
